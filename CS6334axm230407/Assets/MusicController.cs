b9736275e95933de7bc12affa6608eb8d40c19e6
Eswar-deep <f20200378@goa.bits-pilani.ac.in>
2025-04-04 16:59:33 -0500

    updated the music feature
    now playbutton toggles into stop button when music is play and other way around.

--- CS6334axm230407/Assets/MusicController.cs
+++ CS6334axm230407/Assets/MusicController.cs
@@ -1,17 +1,39 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class MusicController : MonoBehaviour
 {
     private AudioSource audioSource;
 
+    [Header("UI Reference")]
+    public TMP_Text toggleButtonText;
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        UpdateButtonText();
     }
 
-    public void PlayMusic()
+    public void ToggleMusic()
     {
-        if (!audioSource.isPlaying)
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        else
+        {
             audioSource.Play();
+        }
+
+        UpdateButtonText();
+    }
+
+    private void UpdateButtonText()
+    {
+        if (toggleButtonText != null)
+        {
+            toggleButtonText.text = audioSource.isPlaying ? "Stop Music" : "Play Music";
+        }
     }
 }
